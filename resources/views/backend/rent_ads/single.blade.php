63417ab38530b4194c326ab78ea9899af1c84f70
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2023-04-27 17:49:51 +0600

    Rent add details page is dynamic now and update part is done

--- resources/views/backend/rent_ads/single.blade.php
+++ resources/views/backend/rent_ads/single.blade.php
@@ -18,71 +18,81 @@ Manage Rent Ad
             <ul class="list-group">
                 <li class="list-group-item d-flex justify-content-center align-items-center flex-column">
                     <div class="rent-ad-image-container">
+                        @if ($rentAd->image != null)
+                        <img src="{{asset('images/rentAds/'.$rentAd->image)}}" alt="ad_image" class="img-fluid">
+                        @else
                         <img src="{{asset('images/no-image.jpg')}}" alt="ad_image" class="img-fluid">
+                        @endif
                     </div>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center flex-lg-row flex-column">
-                  <span class="font-weight-bold">Title:</span>
+                  <span class="font-weight-bold">Title: {{ $rentAd->title }}</span>
                   <span class="text-break"></span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center flex-lg-row flex-column">
                     <span class="font-weight-bold">Price:</span>
-                    <span class="text-break"></span>
+                    <span class="text-break"> {{ $rentAd->price }}</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center flex-lg-row flex-column">
                     <span class="font-weight-bold">Type:</span>
-                    <span class="text-break"></span>
+                    <span class="text-break"> {{ $rentAd->rent_type->type }}</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center flex-lg-row flex-column">
                     <span class="font-weight-bold">Rooms:</span>
-                    <span class="text-break"></span>
+                    <span class="text-break"> {{ $rentAd->rooms }}</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center flex-lg-row flex-column">
                     <span class="font-weight-bold">Bathrooms:</span>
-                    <span class="text-break"></span>
+                    <span class="text-break"> {{ $rentAd->bathrooms }}</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center flex-lg-row flex-column">
                     <span class="font-weight-bold">Floor:</span>
-                    <span class="text-break"></span>
+                    <span class="text-break"> {{ $rentAd->floor }}</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center flex-lg-row flex-column">
                     <span class="font-weight-bold">Status:</span>
                     <span class="text-break">
+                        @if ($rentAd->status == 1)
                         <span class="badge badge-success">Approved</span>
+                        @elseif ($rentAd->status == 2)
                         <span class="badge badge-danger">Rejected</span>
+                        @elseif ($rentAd->status == 0)
                         <span class="badge badge-info">Pending</span>
+                        @else
+                        <span class="badge badge-warning">Modifications Required</span>
+                        @endif
                     </span>
                 </li>
 
                 <li class="list-group-item d-flex flex-column">
                     <span class="font-weight-bold">Address:</span>
-                    <span class="text-break">District + Area + Full Address At once by comma seperated, e.g. District: Dhaka, Area: Badda, Location: Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, doloribus!</span>
+                    <span class="text-break">District: {{ $rentAd->area->district->name }}, Area: {{ $rentAd->area->name }}, Location: {{ $rentAd->full_address }}</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center flex-lg-row flex-column">
                     <span class="font-weight-bold">Posted By:</span>
-                    <span class="text-break"></span>
+                    <span class="text-break">{{ $rentAd->poster->name }}</span>
                 </li>
                 <li class="list-group-item d-flex align-items-center flex-lg-row flex-column">
                     <span class="font-weight-bold">Timeline:</span>
                     <span class="text-break">
-                        <span class="badge badge-primary ml-2">Posted: 1/2/2020</span>
+                        <span class="badge badge-primary ml-2">Posted: {{ date('d/m/y',strtotime($rentAd->created_at)) }}</span>
                     </span>
                     <span class="text-break">
-                        <span class="badge badge-primary ml-2">Updated: 1/2/2020</span>
+                        <span class="badge badge-primary ml-2">Updated: {{ date('d/m/y',strtotime($rentAd->updated_at)) }}</span>
                     </span>
                 </li>
                 <li class="list-group-item d-flex flex-column">
                     <span class="font-weight-bold mb-2">Externals:</span>
                     <span class="text-break">
-                        <small>Video URL:&ensp; <a href="">Link Here</a> </small>
+                        <small>Video URL:&ensp; <a href="{{ $rentAd->video_url }}">Link Here</a> </small>
                     </span>
                     <span class="text-break">
-                        <small>Map URL:&ensp; <a href="">Link Here</a> </small>
+                        <small>Map URL:&ensp; <a href="{{ $rentAd->map_url }}">Link Here</a> </small>
                     </span>
                 </li>
                 <li class="list-group-item d-flex flex-column">
                     <span class="font-weight-bold mb-2">Description:</span>
-                    <span class="text-break"></span>
+                    <span class="text-break">{{ $rentAd->description }}</span>
                 </li>
             </ul>
         </div>
@@ -101,11 +111,33 @@ Manage Rent Ad
                 <div class="form-group">
                     <label for="status">Status</label>
                     <select name="status" id="status" class="form-control">
+                        @if ($rentAd->status == 0)
+                        <option value="0" selected>Pending</option>
+                        <option value="1">Approved</option>
+                        <option value="2">Rejected</option>
+                        <option value="3">Modifications Required</option>
+                        @elseif ($rentAd->status == 1)
+                        <option value="0">Pending</option>
+                        <option value="1" selected>Approved</option>
+                        <option value="2">Rejected</option>
+                        <option value="3">Modifications Required</option>
+                        @elseif ($rentAd->status == 2)
+                        <option value="0">Pending</option>
+                        <option value="1">Approved</option>
+                        <option value="2" selected>Rejected</option>
+                        <option value="3">Modifications Required</option>
+                        @elseif ($rentAd->status == 3)
+                        <option value="0">Pending</option>
+                        <option value="1">Approved</option>
+                        <option value="2">Rejected</option>
+                        <option value="3" selected>Modifications Required</option>
+                        @else
                         <option value="">--SELECT--</option>
                         <option value="1">Approved</option>
                         <option value="2">Pending</option>
                         <option value="0">Rejected</option>
                         <option value="3">Modifications Required</option>
+                        @endif
                     </select>
                 </div>
                 <div class="form-group mt-3">
