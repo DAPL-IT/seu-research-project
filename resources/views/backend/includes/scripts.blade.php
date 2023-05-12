fc2fa6f25e34d4406cb13857622c68a46292dc04
salehibneomar <salehibneomar@gmail.com>
2023-05-13 04:53:06 +0600

    test

--- resources/views/backend/includes/scripts.blade.php
+++ resources/views/backend/includes/scripts.blade.php
@@ -14,6 +14,9 @@
 ')}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="{{asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('backend/js/demo/chart-pie-demo.js')}}"></script>
+<script src="{{asset('backend/js/demo/chart-bar-demo.js')}}"></script>
 
 <script>
 $(document).ready(()=>{
