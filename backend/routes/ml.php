0e066ece307ede12bab5931fea80023e6d0c40dc
salehibneomar <salehibneomar@gmail.com>
2023-06-11 04:25:54 +0600

    ML model made and saved in public dir ml_model folder, ML algorithm impl. is done, Search controller has been created, only api calling is remaining and frontend

---
+++ backend/routes/ml.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\ML\MlModelController;
+use Illuminate\Support\Facades\Route;
+
+//** DO NOT WORK HERE This is a private route page to test ML and making Models */
+
+// Route::get('/ml-test', [MlModelController::class, 'index']);
+// Route::get('/ml-test-cases', [MlModelController::class, 'performTestRequest']);
+// Route::get('/ml-test-cases-values', [MlModelController::class, 'getTestCases']);
+// Route::get('/test', [MlModelController::class, 'show']);
+
+// Route::get('/ml-test-lr-data', [MlModelController::class, 'getDemoDataForLinearRegression']);
+// Route::get('/ml-test-knn-data', [MlModelController::class, 'getDemoDataForKnn']);
+// Route::get('/ml-test-price-ranges', [MlModelController::class, 'getDemoPriceRanges']);
+// Route::get('/ml-test-train-save', [MlModelController::class, 'trainAndSave']);
