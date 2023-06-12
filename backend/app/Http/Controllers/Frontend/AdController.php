a7b615c911d8a8c9c4597899defd3d92bfef403b
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2023-06-12 20:15:24 +0600

    Create add is done

--- backend/app/Http/Controllers/Frontend/AdController.php
+++ backend/app/Http/Controllers/Frontend/AdController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\RentAd;
+use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class AdController extends Controller
 {
@@ -50,6 +55,52 @@ class AdController extends Controller
             $response = ['validation' => $validator->errors()];
             return response()->json($response, 422);
         }
+
+        $user = Auth::user();
+        $rent_ad = new RentAd();
+        $rent_ad->title = $request->title;
+        $rent_ad->price = $request->price;
+        $rent_ad->rooms = $request->rooms;
+        $rent_ad->bathrooms = $request->bathrooms;
+        $rent_ad->floor = $request->floor;
+        $rent_ad->square_feet = $request->square_feet;
+        $rent_ad->full_address = $request->full_address;
+        $rent_ad->description = $request->description;
+        $rent_ad->is_featured = $request->is_featured;
+        $rent_ad->sold_on_site = $request->sold_on_site;
+        $rent_ad->map_url = $request->map_url;
+        $rent_ad->video_url = $request->video_url;
+        $rent_ad->rent_type_id  = $request->rent_type_id;
+        $rent_ad->area_id = $request->area_id;
+        $rent_ad->poster_id = $user->id;
+
+        //Assign Random moderator...
+        $moderatorIds = User::where('role', 'moderator')->where('locked', 0)->pluck('id');
+        $randomModeratorId = User::select(DB::raw('FLOOR(RAND() * COUNT(*)) as random_id'))->value('random_id');
+        $randomModerator = User::where('id', $randomModeratorId)->first();
+        if($randomModerator){
+            $rent_ad->moderator_id  = $randomModerator->id;
+        }
+        //Assign Random moderator...
+
+        //Image Upload...
+        if($request->hasFile('image')){
+            $location  = RentAd::IMAGE_DIR;
+            $imageFile = $request->file('image');
+            $imageName = Str::random(16).'_dt_'.date('Ymd_His').'.'.$imageFile->getClientOriginalExtension();
+
+            Image::make($imageFile)
+            ->fit(150,150, function($constraint){
+                $constraint->upsize();
+            })->save($location.$imageName);
+
+            $rent_ad->image = $location.$imageName;
+        }
+        //Image Upload...
+
+        $rent_ad->save();
+
+        return response()->json(['message' => 'Created Successfully',], 200);
     }
 
 }
